54c3912b3b1d67372074d1ef13fd0dd1d9efacab
Erik Schluntz <eschluntz@gmail.com>
2015-11-13 16:04:54 -0500

    server stuff

---
+++ fabfile.py
@@ -0,0 +1,25 @@
+import os.path
+from fabric.api import *
+
+def ocean():
+    env.hosts = ['107.170.6.59']
+    env.user = 'django'
+
+def pull():
+    sudo('cd /home/django/zipbikes; git pull;') # runs the command on the remote environment 
+
+def enter():
+    run('cd /home/django/zipbikes/server/; source env/bin/activate;')
+
+def sync():
+    run('cd /home/django/zipbikes/server/zipbikes; ./manage.py syncdb')
+    run('cd /home/django/zipbikes/server/zipbikes; ./manage.py migrate')
+
+def reload():
+    sudo('service gunicorn restart')
+
+def deploy():
+    pull()
+    enter()
+    sync()
+    reload()
